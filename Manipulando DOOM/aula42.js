6275241a7c6de8017d28abbcbb20bd42d049a4e1
Arthur <arthurmouramachado@gmail.com>
2025-09-11 15:02:25 -0300

    Novo Arquivo Adicionado

--- Manipulando DOOM/aula42.js
+++ Manipulando DOOM/aula42.js
@@ -1,3 +1,17 @@
 //metodo filter
 const idades=[15,21,30,17,18,44,12,50]
-const maior=idades.filter()
+const maior=idades.filter((v1)=>{
+    if(v1 >=18){
+        return v1;
+    }
+})
+
+const menor=idades.filter((v2)=>{
+    if(v2 <18){
+        return v2;
+    }
+})
+
+console.log(idades)
+console.log(maior)
+console.log(menor)
