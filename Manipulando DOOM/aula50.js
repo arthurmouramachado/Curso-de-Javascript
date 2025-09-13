48f5cb7bc7ca549d4a31f66b2c31e48793364abd
Arthur <arthurmouramachado@gmail.com>
2025-09-13 16:32:38 -0300

    Novas Aulas

--- Manipulando DOOM/aula50.js
+++ Manipulando DOOM/aula50.js
@@ -3,10 +3,17 @@ const btnReduzir=document.querySelector("#btnReduzir");
 const resultado=document.querySelector("#resultado");
 
 const elementos_array=[1,2,3,4,5]
-let aux=[];
+let ant=[];
+let atu=[];
 
 p_array.innerHTML="[" + elementos_array + "]"
 
 btnReduzir.addEventListener("click",(evt)=>{
+    resultado.innerHTML=elementos_array.reduce((anterior, atual,pos)=>{
+        ant.push(anterior)
+        atu.push(atual)
+        return atual+anterior;
+    })
+    resultado.innerHTML+="<br/>Anterior: "+ant+"<br/>Atual: "+atu
     
 })
